3ce51ad266186b9752f0b257ce149390b58d5b8f
Pedro Correa <pedrocorrea.85@hotmail.com>
2020-02-11 23:10:55 -0300

    Adjust Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `aida-medical`,
+        short_name: `aidamedical`,
         start_url: `/`,
         background_color: `#00A099`,
         theme_color: `00A099`,
